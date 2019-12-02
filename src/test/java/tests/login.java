3289d3c37c66f069b657f04bc164b2798fff0942
Umit Uzulmez <umituzulmez@gmail.com>
2019-12-02 20:20:02 +0000

    updated 2 files

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -4,6 +4,6 @@ public class login {
 
     public static void main(String[] args) {
 
-        System.out.println("User log in");
+
     }
 }
--- src/test/java/tests/logout.java
+++ src/test/java/tests/logout.java
@@ -2,5 +2,8 @@ package tests;
 
 public class logout {
 
+    public static void main(String[] args) {
 
+        //This is something I added
+    }
 }
