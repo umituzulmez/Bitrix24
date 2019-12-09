0f076c3bff6e6822c13228847041547ba82a6cb7
umituzulmez <55238370+umituzulmez@users.noreply.github.com>
2019-12-09 18:18:21 +0000

    added return method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -3,11 +3,12 @@ package tests;
 public class task {
 
 public static void main(String[] args){
- 
   System.out.println("Hello World");
-  
   //this line is added from GitHub, see you in Intellij
 
 }
-  
+ public String Hello(){
+  return "Hello from GitHub";
+}
+
 }
