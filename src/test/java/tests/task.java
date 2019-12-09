dba457516511274cf97409381e04305516cb8f12
umituzulmez <55238370+umituzulmez@users.noreply.github.com>
2019-12-09 17:37:49 +0000

    updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -2,5 +2,12 @@ package tests;
 
 public class task {
 
+public static void main(String[] args){
+ 
+  System.out.println("Hello World");
+  
+  //this line is added from GitHub, see you in Intellij
 
+}
+  
 }
